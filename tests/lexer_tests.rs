9255c6f197d7e18ee5a1e106ccf032ecec591b70
fabiovandewaeter <vandewaeter.fabio@gmail.com>
2025-05-31 15:28:23 +0200

    add lexer test for function declaration

--- tests/lexer_tests.rs
+++ tests/lexer_tests.rs
@@ -3,7 +3,7 @@ use tricot_lang::lexer::Token;
 
 #[test]
 fn test_lexer_basic() {
-    let input = "let mut x = 42;";
+    let input = "let mut x = 42";
     let mut lexer = Token::lexer(input);
 
     assert_eq!(lexer.next(), Some(Ok(Token::Let)));
@@ -11,5 +11,31 @@ fn test_lexer_basic() {
     assert_eq!(lexer.next(), Some(Ok(Token::Identifier("x".into()))));
     assert_eq!(lexer.next(), Some(Ok(Token::Assign)));
     assert_eq!(lexer.next(), Some(Ok(Token::Number("42".into()))));
-    assert_eq!(lexer.next(), Some(Ok(Token::Semicolon)));
+}
+
+#[test]
+fn test_lexer_function_definition() {
+    let input = "fn add(a: Int, b: Int) -> Int {
+        a + b
+    }";
+    let mut lexer = Token::lexer(input);
+
+    assert_eq!(lexer.next(), Some(Ok(Token::Fn)));
+    assert_eq!(lexer.next(), Some(Ok(Token::Identifier("add".into()))));
+    assert_eq!(lexer.next(), Some(Ok(Token::ParenthesisOpen)));
+    assert_eq!(lexer.next(), Some(Ok(Token::Identifier("a".into()))));
+    assert_eq!(lexer.next(), Some(Ok(Token::Colon)));
+    assert_eq!(lexer.next(), Some(Ok(Token::IntType)));
+    assert_eq!(lexer.next(), Some(Ok(Token::Comma)));
+    assert_eq!(lexer.next(), Some(Ok(Token::Identifier("b".into()))));
+    assert_eq!(lexer.next(), Some(Ok(Token::Colon)));
+    assert_eq!(lexer.next(), Some(Ok(Token::IntType)));
+    assert_eq!(lexer.next(), Some(Ok(Token::ParenthesisClose)));
+    assert_eq!(lexer.next(), Some(Ok(Token::Arrow)));
+    assert_eq!(lexer.next(), Some(Ok(Token::IntType)));
+    assert_eq!(lexer.next(), Some(Ok(Token::CurlyBraceOpen)));
+    assert_eq!(lexer.next(), Some(Ok(Token::Identifier("a".into()))));
+    assert_eq!(lexer.next(), Some(Ok(Token::Plus)));
+    assert_eq!(lexer.next(), Some(Ok(Token::Identifier("b".into()))));
+    assert_eq!(lexer.next(), Some(Ok(Token::CurlyBraceClose)));
 }
